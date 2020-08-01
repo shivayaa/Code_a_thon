a050043e6346fdf3b7878e5864c7dc8e89830cc8
shiv prakash yadav <shivaofficial786@gmail.com>
2020-08-01 13:59:35 +0530

    Code_a_thon

---
+++ quoteapp/views.py
@@ -0,0 +1,33 @@
+from django.shortcuts import render
+
+# Create your views here.
+def home(request):
+
+    return render(request,'quote/home.html')
+
+def About(request):
+
+    return render(request,'quote/about.html')
+
+def Contact(request):
+
+    return render(request,'quote/contact.html')
+
+def Dashboard(request):
+
+
+    return render(request,'quote/dashboard.html')
+
+def user_logout(request):
+
+    
+    return redirect("/")
+
+def Singup(request):
+
+    
+    return render(request,'quote/singup.html')
+
+def Login(request):
+
+    return render(request,'quote/login.html')
